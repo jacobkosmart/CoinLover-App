5ad2c37f19356d7d14aa07c836b9d1026e1a6a5a
jacobkosmart <jacobkosmart@gmail.com>
2022-05-19 14:58:07 +0900

    init project

--- CoinLover/ContentView.swift
+++ CoinLover/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		ZStack {
+			Color.theme.background.ignoresSafeArea()
+			
+			VStack (spacing: 20) {
+				Text("Accent Color")
+					.foregroundColor(Color.theme.accent)
+				
+				Text("Seconday Text Color")
+					.foregroundColor(Color.theme.secondaryText)
+				
+				Text("Red Color")
+					.foregroundColor(Color.theme.red)
+				
+				Text("Green Color")
+					.foregroundColor(Color.theme.green)
+			}
+			.font(.headline)
+			
+		} //: ZSTACK
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+			.preferredColorScheme(.dark)
+	}
 }
