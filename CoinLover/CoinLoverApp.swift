f1e59eef83bc291b442bd33078700720d9b30465
jacobkosmart <jacobkosmart@gmail.com>
2022-05-19 14:02:36 +0900

    Initial Commit

---
+++ CoinLover/CoinLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinLoverApp.swift
+//  CoinLover
+//
+//  Created by Jacob Ko on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct CoinLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
