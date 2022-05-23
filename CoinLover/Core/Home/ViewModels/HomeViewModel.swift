c335ce98f7e76bd2c23a70a6c96f89a176827e0e
jacobkosmart <jacobkosmart@gmail.com>
2022-05-23 21:24:21 +0900

    11.Filtering data based on search bar text using Combine

--- CoinLover/Core/Home/ViewModels/HomeViewModel.swift
+++ CoinLover/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,10 +23,30 @@ class HomeViewModel: ObservableObject {
 	}
 	// MARK: -  FUNCTION
 	func addSubscribers() {
-		dataService.$fetchedCoins
+		
+		// update allCoins
+		$searchText
+			.combineLatest(dataService.$fetchedCoins)
+		// debonce will wait 0.5 sec before running the rest of this code
+		// some of user types letter quickly, so debounce make the search term
+			.debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+			.map(filterCoins)
 			.sink { [weak self] (returnedCoins) in
 				self?.allCoins = returnedCoins
 			}
 			.store(in: &cancellables)
+	}
+	
+	private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+		guard !text.isEmpty else {
+			return coins
+		}
+		let lowercasedText = text.lowercased()
+		
+		let filteredCoins = coins.filter { (coin) -> Bool in
+			return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) || coin.id.lowercased().contains(lowercasedText)
+			
+		}
+		return filteredCoins
 	}
 }
